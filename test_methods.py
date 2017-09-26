b5ed37020d4f863652f405712a57fc416e81c8c3
bryan <bgrr05@gmail.com>
2017-09-26 11:16:01 -0500

    testMethods commit

--- test_methods.py
+++ test_methods.py
@@ -2,6 +2,8 @@ import pyaudio
 
 p = pyaudio.PyAudio()
 
+usb_dev = 0
+
 for i in range(p.get_device_count()):
 	if "USB" in p.get_device_info_by_index(i)['name']:
 		usb_dev = p.get_device_info_by_index(i)['index']
