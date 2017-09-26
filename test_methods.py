eb305a0f7db19160db6090c3846ad73b57e3a743
bryan <bgrr05@gmail.com>
2017-09-26 11:31:52 -0500

    testMethods commit

--- test_methods.py
+++ test_methods.py
@@ -3,11 +3,16 @@ import pyaudio
 p = pyaudio.PyAudio()
 
 
-
 def get_usb_index():
 	for i in range(p.get_device_count()):
-		if "USB" in p.get_device_info_by_index(i)['name']:
+		if "HDMI" in p.get_device_info_by_index(i)['name']:
 			return p.get_device_info_by_index(i)['index']
 
+def get_audio_devices():
+	for i in range(p.get_device_count()):
+		print(p.get_device_info_by_index(i)['name'])
 
 
+get_audio_devices()
+
+print(get_usb_index())
