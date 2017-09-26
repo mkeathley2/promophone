4c51dfd3d82638ba41745222ccfe21523bdea296
bryan <bgrr05@gmail.com>
2017-09-26 14:48:37 -0500

    testMethods commit

--- core.py
+++ core.py
@@ -59,10 +59,16 @@ def CheckForUpdate():
 
         if os.path.isfile(update_path_full):
             shutil.copy(update_path_full, promo_file_location)
-            print("Copied Files Success!!*****@@@@")
-        else:
-            print("PromoPhone.mp3 not found. Make sure file is on root of flash drive.")
 
+            print("Copied Files Success!!*****@@@@")
+            play_update_success()
+            return
+        else:
+
+            print("PromoPhone.mp3 not found. Make sure file is on root of flash drive.")
+            continue
+
+    play_missing_file_error()
     return
 
 
@@ -70,3 +76,27 @@ def get_usb_index():
     for i in range(p.get_device_count()):
         if "USB" in p.get_device_info_by_index(i)['name']:
             return p.get_device_info_by_index(i)['index']
+
+
+def play_update_success():
+    pygame.mixer.init()
+    pygame.mixer.music.load("UpdateSuccess.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        if hook.is_pressed:
+            pygame.time.Clock().tick(10)
+        else:
+            OnHook()
+            break
+
+def play_missing_file_error():
+    pygame.mixer.init()
+    pygame.mixer.music.load("UpdateError.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        if hook.is_pressed:
+            pygame.time.Clock().tick(10)
+        else:
+            OnHook()
+            break
+
