2e03a2571710830b83fe025da4dc37d7cea72a2e
bryan <bgrr05@gmail.com>
2017-09-27 09:39:16 -0500

    Merge branches 'bryan' and 'matt' of github.com:mkeathley2/promophone into bryan
    Conflicts:
    	Main.py
    	core.py

--- Main.py
+++ Main.py
@@ -2,23 +2,21 @@ import core
 from datetime import datetime
 from time import sleep
 
-hook = core.hook
-
 while True:
     if datetime.now().hour >= 11 and datetime.now().hour < 14:
         Frequency = core.Lunch_Frequency
     else:
         Frequency = core.Default_Frequency
 
-    if hook.is_pressed:
+    if core.hook.is_pressed:
         core.CheckForUpdate()
-    while hook.is_pressed:
+    while core.hook.is_pressed:
         core.OffHook()
         break
 
-    # # RING TEST CODE. DON'T FORGET TO COMMENT OUT
-    if datetime.now().minute in [31, 32]:
-        core.Ring()
+    # # # RING TEST CODE. DON'T FORGET TO COMMENT OUT
+    # if datetime.now().minute in [31, 32]:
+    #     core.Ring()
 
     if datetime.now().minute % Frequency == 0 and datetime.now().second == 10:
         core.Ring()
